4b1b34a4e47b80058137e2866ce6abff1cc44449
alexis <alexis.auriac@epitech.eu>
2020-02-10 14:59:34 +0100

    Added the span function

--- src/Medium1.hs
+++ src/Medium1.hs
@@ -1,6 +1,7 @@
 module Medium1 (
     myTake,
     myDrop,
+    mySpan,
     myTakeWhile,
     myDropWhile
 ) where
@@ -13,17 +14,17 @@ myDrop 0 xs = xs
 myDrop _ [] = []
 myDrop n (_:xs) = myDrop (n-1) xs
 
-myTakeWhile' :: (a -> Bool) -> [a] -> [a] -> [a]
-myTakeWhile' _ [] res = reverse res
-myTakeWhile' p (x:xs) res
-    | p x = myTakeWhile' p xs (x:res)
-    | otherwise = reverse res
+mySpan' :: (a -> Bool) -> [a] -> [a] -> ([a], [a])
+mySpan' _ [] left = (reverse left, [])
+mySpan' p (x:xs) left
+    | p x = mySpan' p xs (x:left)
+    | otherwise = (reverse left, x:xs)
+
+mySpan :: (a -> Bool) -> [a] -> ([a], [a])
+mySpan p xs = mySpan' p xs []
 
 myTakeWhile :: (a -> Bool) -> [a] -> [a]
-myTakeWhile p xs = myTakeWhile' p xs []
+myTakeWhile p xs = fst $ mySpan p xs
 
 myDropWhile :: (a -> Bool) -> [a] -> [a]
-myDropWhile _ [] = []
-myDropWhile p (x:xs)
-    | p x = myDropWhile p xs
-    | otherwise = x:xs
+myDropWhile p xs = snd $ mySpan p xs
